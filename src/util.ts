30f9f025f4adafc6d01d1bee2bf260e8faf723d1
Joe Cross <joe.mcross@gmail.com>
2021-03-18 20:17:26 -0700

    add util.index fn

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,5 @@
 /**
  * [1,2,3] ∪ [3,4,5] => Set([1,2,3,4,5])
- * @param {Array<T>} a first array
- * @param {Array<T>} b second array
- * @returns {Set<T>} elements in either array
  */
 export function setUnion<T>(a: Array<T>, b: Array<T>): Set<T> {
   // https://exploringjs.com/impatient-js/ch_sets.html#union-a-b
@@ -11,28 +8,22 @@ export function setUnion<T>(a: Array<T>, b: Array<T>): Set<T> {
 
 /**
  * [1,2,3] ∩ [3,4,5] => Set([3])
- * @param {Array<T>} a first array
- * @param {Array<T>} b second array
- * @returns {Set<T>} elements that are in both arrays
  */
 export function setIntersection<T>(a: Array<T>, b: Array<T>): Set<T> {
   // https://exploringjs.com/impatient-js/ch_sets.html#intersection-a-b
   // ensure shortest array is first
   if (b.length < a.length) [a, b] = [b, a]
   const smallSet = new Set(a)
-  return new Set(b.filter(x => smallSet.has(x)))
+  return new Set(b.filter((x) => smallSet.has(x)))
 }
 
 /**
  * [1,2,3] \ [3,4,5] => Set([1,2])
- * @param {Array<T>} a first array
- * @param {Array<T>} b second array
- * @returns {Set<T>} elements in the first array but not the second
  */
 export function setDifference<T>(a: Array<T>, b: Array<T>): Set<T> {
   // https://exploringjs.com/impatient-js/ch_sets.html#difference-a-b
   const bSet = new Set(b)
-  return new Set(a.filter(x => !bSet.has(x)))
+  return new Set(a.filter((x) => !bSet.has(x)))
 }
 
 /**
@@ -41,9 +32,19 @@ export function setDifference<T>(a: Array<T>, b: Array<T>): Set<T> {
  * @example
  * let a = [3, null, 2, undefined, 1, 0]
  * let b: number[] = a.filter(isDefined)  // [3, 2, 1, 0]
- * @param {T} t possibly defined object
- * @returns {boolean} true if t is not null or undefined
  */
 export function isDefined<T>(t: T | null | undefined): t is T {
   return t !== undefined && t !== null
+}
+
+/**
+ * construct a new object from an array of values, indexed by a given function.
+ * index([0,1,2], (n)=>`hi.${n}`) => {'hi.0':0, 'hi.1':1, 'hi.2':2}
+ */
+export function index<TKey extends string | number, TValue>(
+  values: TValue[],
+  id: (v: TValue) => TKey,
+): { [K in TKey]: TValue } {
+  const entries = values.map((v) => [id(v), v])
+  return Object.fromEntries(entries)
 }
